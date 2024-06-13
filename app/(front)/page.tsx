a9970aff6330186407157d13443224a3d58375b6
moamer-dev <mohamed.awad.amer@gmail.com>
2024-06-13 13:08:05 +0200

    feat: adding a hero section

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Hero from "@/components/Frontend/Hero";
 
 export default function Home() {
   return (
-    <>
-      {" "}
-      <div className="container">Home</div>
-    </>
+    <section className="max-w-6xl mx-auto">
+      <Hero />
+    </section>
   );
 }
---
+++ components/Frontend/Hero.tsx
@@ -0,0 +1,128 @@
+"use client";
+import React, { useState } from "react";
+
+const Hero = () => {
+  return (
+    <div className="relative e pb-[110px] pt-[120px] dark:bg-dark lg:pt-[150px] p-10">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 lg:w-5/12">
+            <div className="hero-content">
+              <h1 className="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl">
+                <span className="text-indigo-600">TheraBook</span> <br /> Book a
+                Doctor in <br /> Seconds
+              </h1>
+              <p className="mb-8 max-w-[480px] text-base text-body-color dark:text-dark-6">
+                TheraBook is a platform that connects you to the best doctors in
+                your area. We make it easy to book an appointment with a doctor
+                in seconds.
+              </p>
+              <div className="mt-6 flex max-w-md gap-x-4">
+                <label htmlFor="email-address" className="sr-only">
+                  Search for a Doctor name
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 shadow-md ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-100 sm:text-sm sm:leading-6"
+                  placeholder="Search for a Doctor name"
+                />
+                <button
+                  type="submit"
+                  className="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Search
+                </button>
+              </div>
+              <div className="clients pt-16">
+                <h6 className="mb-6 flex items-center text-xs font-normal text-body-color dark:text-dark-6">
+                  Some of partner Clinics
+                  <span className="ml-3 inline-block h-px w-8 bg-body-color"></span>
+                </h6>
+
+                <div className="flex items-center space-x-4">
+                  <SingleImage
+                    href="#"
+                    imgSrc="https://cdn.tailgrids.com/2.0/image/assets/images/brands/ayroui.svg"
+                  />
+
+                  <SingleImage
+                    href="#"
+                    imgSrc="https://cdn.tailgrids.com/2.0/image/assets/images/brands/graygrids.svg"
+                  />
+
+                  <SingleImage
+                    href="#"
+                    imgSrc="https://cdn.tailgrids.com/2.0/image/assets/images/brands/uideck.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="hidden px-4 lg:block lg:w-1/12"></div>
+          <div className="w-full px-4 lg:w-6/12">
+            <div className="lg:ml-auto lg:text-right">
+              <div className="relative z-10 inline-block pt-11 lg:pt-0">
+                <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
+                  alt="hero"
+                  className="max-w-full lg:ml-auto"
+                />
+                <span className="absolute -bottom-8 -left-8 z-[-1]">
+                  <svg
+                    width="93"
+                    height="93"
+                    viewBox="0 0 93 93"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+                  </svg>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
+
+const SingleImage = ({ href, imgSrc }: { href: string; imgSrc: string }) => {
+  return (
+    <>
+      <a href={href} className="flex w-full items-center justify-center">
+        <img src={imgSrc} alt="brand image" className="h-10 w-full" />
+      </a>
+    </>
+  );
+};

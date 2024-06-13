288d5f4adf3d066cf7508f4fe687019cf4fd8453
moamer-dev <mohamed.awad.amer@gmail.com>
2024-06-13 13:07:25 +0200

    fix: navbar submenu z-index

--- components/Frontend/NavBar.tsx
+++ components/Frontend/NavBar.tsx
@@ -115,7 +115,7 @@ export default function NavBar() {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <PopoverPanel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+              <PopoverPanel className="z-50 absolute -left-8 top-full  mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
                 <div className="p-4">
                   {products.map((item) => (
                     <div
